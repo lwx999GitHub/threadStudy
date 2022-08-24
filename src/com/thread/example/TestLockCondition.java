bee3d51d9cd45513ecf11e9f587983b6554a99e4
jane <lvwx86@163.com>
2022-08-24 20:56:12 +0800

    线程学习(TestLockCondition)

---
+++ src/com/thread/example/TestLockCondition.java
@@ -0,0 +1,66 @@
+package com.thread.example;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLockCondition {
+
+    static Thread t1 = null, t2 = null;
+
+    public static void main(String[] args) {
+        final Object o = new Object();
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        t1 = new Thread(() -> {
+            try {
+                lock.lock();
+                for (char c : aI) {
+                    System.out.print(c);
+                    try {
+                        condition.signal();
+                        condition.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                condition.signal();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+        }, "t1");
+
+        t2 = new Thread(() -> {
+            try {
+                lock.lock();
+                for (char c : aC) {
+                    System.out.print(c);
+                    try {
+                        condition.signal();
+                        condition.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                condition.signal();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+        }, "t2");
+
+        t1.start();
+        t2.start();
+
+    }
+}
